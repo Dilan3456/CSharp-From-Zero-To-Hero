342174a016b2c97bbda68b01604bd98e18ce2e9c
Dilan <dilansvega@gmail.com>
2023-11-15 19:10:14 -0500

    Line to describe the end of the program

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,6 +7,8 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to close the program .....");
+            Console.ReadLine();
         }
     }
 }
